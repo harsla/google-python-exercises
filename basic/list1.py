436c57f4703e014badcc865be4017cc627a14b18
Scott <harsla@gmail.com>
2014-12-27 20:13:53 -0700

    match_ends

--- basic/list1.py
+++ basic/list1.py
@@ -21,8 +21,11 @@
 # and last chars of the string are the same.
 # Note: python does not have a ++ operator, but += works.
 def match_ends(words):
-  # +++your code here+++
-  return
+  i = 0
+  for word in words:
+      if len(word) >= 2 and word[0] == word [-1]:
+          i +=1
+  return i
 
 
 # B. front_x
